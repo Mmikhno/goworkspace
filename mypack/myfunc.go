bbdb81ab86629d44cf0f289e22550b442a1668c3
Maria Mikhno <mmikhno89@gmail.com>
2025-12-02 20:17:13 +0300

    second version

--- mypack/myfunc.go
+++ mypack/myfunc.go
@@ -1,12 +1,24 @@
 // пакет предоставляет функию для умножения двух чисел
 package mypack
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
 // функция Add принимает два целых числа,
 // находит их сумму и возвращает результат
 //
 // подробности можно найти [тут]
 //
 // [тут]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a,b int) int{
+
+type Number interface{
+	constraints.Integer | constraints.Float
+}
+// func Add(a,b int) int{
+// 	return a + b
+// } 
+func Add[T Number] (a, b T) T{
 	return a + b
-} 
+}
+
